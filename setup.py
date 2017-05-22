2d5104ea73cfb1947eeece9d6c57fa4c67561770
Ratnani, Ahmed (ahr) <ahmed.ratnani@ipp.mpg.de>
2017-05-22 09:10:01 +0200

    improve setup

--- setup.py
+++ setup.py
@@ -1,10 +1,53 @@
-from setuptools import setup
+# -*- coding: UTF-8 -*-
+#! /usr/bin/python
 
-setup(name='symcc',
-        version='0.0',
-        description='Symbolic Mathematics Compiler',
-        author='Jim Crist',
-        install_requires=['sympy>=0.7.5-git'],
-        tests_require=['pytest'],
-        dependency_links = ['http://github.com/sympy/sympy/tarball/master#egg=sympy-0.7.5-git']
+import sys
+from numpy.distutils.core import setup
+
+NAME    = 'symcc'
+VERSION = '0.1'
+AUTHOR  = 'Jim Crist, Ahmed Ratnani'
+EMAIL   = 'ahmed.ratnani@ipp.mpg.de'
+URL     = 'https://github.com/ratnania/symcc'
+DESCR   = 'Symbolic Mathematics Compiler'
+KEYWORDS = ['math']
+LICENSE = "LICENSE"
+
+setup_args = dict(
+    name             = NAME,
+    version          = VERSION,
+    description      = DESCR,
+    long_description = open('README.md').read(),
+    author           = AUTHOR,
+    author_email     = EMAIL,
+    license          = LICENSE,
+    keywords         = KEYWORDS,
+    url              = URL,
+#    download_url     = URL+'/tarball/master',
 )
+
+
+# ...
+packages=[  'symcc' \
+          , 'symcc.printers' \
+          , 'symcc.types' \
+          , 'symcc.utilities' \
+         ]
+package_dir={  'symcc':                'symcc' \
+              ,'symcc.printers':       'symcc/printers' \
+              ,'symcc.types':          'symcc/types' \
+              ,'symcc.utilities':      'symcc/utilities' \
+              ,}
+# ...
+
+
+def setup_package():
+    if 'setuptools' in sys.modules:
+        setup_args['install_requires'] = ['numpy']
+    setup(  packages = packages \
+          , package_dir=package_dir \
+          , **setup_args)
+
+
+if __name__ == "__main__":
+    setup_package()
